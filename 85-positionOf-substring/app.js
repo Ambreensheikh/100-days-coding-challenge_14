cffa5c733dc7a8814a876732fe4a60f9ef9ce8d4
Ambreen Sheikh <ambreensheikh900@gmail.com>
2024-04-03 20:37:37 +0500

    first commit

---
+++ 85-positionOf-substring/app.js
@@ -0,0 +1,6 @@
+"use strict";
+//find the text "code" in a function
+function findCode(text) {
+    return text.indexOf("code");
+}
+console.log(findCode("read with typescript code easily"));
---
+++ 86-check-for-mytext/app.js
@@ -0,0 +1,7 @@
+"use strict";
+//create a function
+function detectJavascript(text) {
+    return text.includes("Javascript");
+}
+//use .includes to find text t/f
+console.log(detectJavascript("i love to learn Javascript"));
